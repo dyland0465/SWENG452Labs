81968fb66e99b0f2c871d268c8dd3bd97151336f
dyland0465 <152417611+dyland0465@users.noreply.github.com>
2026-03-03 23:25:06 -0500

    Modify socket address and implement task functions
    Updated IP address for socket connection and added functionality for task handling in Lab7.

--- SWENG452Labs/lab4/lab7/Lab7.c
+++ SWENG452Labs/lab4/lab7/Lab7.c
@@ -7,7 +7,6 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 // our generic function pointer prototype
 typedef void *(*FunctionPointer)(double *, int *, char *, char *);
 // struct definition for a node in our linked list
@@ -16,6 +15,7 @@ typedef struct fcnNode {
   struct fcnNode *nextNode;
 } fcnNode;
 
+// six functions for our six system tasks
 void *T1(double *, int *, char *, char *);
 void *T2(double *, int *, char *, char *);
 void *T3(double *, int *, char *, char *);
@@ -23,14 +23,14 @@ void *T4(double *, int *, char *, char *);
 void *T5(double *, int *, char *, char *);
 void *T6(double *, int *, char *, char *);
 
-// six functions for our six system tasks
-//
-// Task 1 - Sensor Data
 void *T1(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {
   // Socket Stuff
   int socketfd, len, result, BUF_SIZE = 1;
   struct sockaddr_in address;
   char buffer;
+
+  int s_flag = (*regVal & 0x01);
+
   socketfd = socket(AF_INET, SOCK_STREAM, 0);
   if (socketfd == -1) {
     printf("socket creation failed...\n");
@@ -41,7 +41,7 @@ void *T1(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {
   // address if both server and client are running on same machine
   // address.sin_addr.s_addr = inet_addr("127.0.0.1");
   // address of server’s IP on network – changed this to your server IP!!
-  address.sin_addr.s_addr = inet_addr("172.29.184.3");
+  address.sin_addr.s_addr = inet_addr("192.168.1.35");
   address.sin_port = htons(2020);
   len = sizeof(address);
   // Read SSR register value
@@ -67,7 +67,7 @@ void *T1(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {
   // address if both server and client are running on same machine
   // address.sin_addr.s_addr = inet_addr("127.0.0.1");
   // address of server’s IP on network – changed this to your server IP!!
-  address.sin_addr.s_addr = inet_addr("172.29.184.3");
+  address.sin_addr.s_addr = inet_addr("192.168.1.35");
   address.sin_port = htons(2000);
   len = sizeof(address);
   result = connect(socketfd, (struct sockaddr *)&address, len);
@@ -81,15 +81,76 @@ void *T1(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {
   // update our temp value back in main
   *tempVal = buffer;
   // return value isn't used, but good practice to return a value
+
+  if (s_flag) {
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    connect(socketfd, (struct sockaddr *)&address, len);
+    read(socketfd, &buffer, BUF_SIZE);
+    close(socketfd);
+
+    *tempVal = buffer;
+  } else {
+    *tempVal = 0;
+  }
+
   return NULL;
 }
 
-void *T2(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {}
+void *T2(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {
 
-void *T3(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {}
-void *T4(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {}
-void *T5(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {}
-void *T6(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {}
+  int l_flag = (*regVal & 0x02);
+
+  if (l_flag) {
+    printf("Acknowledge: Left button on\n ");
+  }
+
+  return NULL;
+}
+void *T3(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {
+  int t_flag = (*regVal & 0x10);
+  *currentTemp = (double)(*tempVal);
+
+  if (t_flag) {
+    *currentTemp = (*currentTemp * 9 / 5) + 32;
+  }
+  return NULL;
+}
+
+void *T4(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {
+  char unit = (*regVal & 0x10) ? 'F' : 'C';
+
+  printf("RB: %s Buzzer: %s MB: %s Sensor: %s LB: %s Temperature: %6.2f %c\n",
+         (*regVal & 0x08) ? "on " : "off", // Right Button bit
+         (*regVal & 0x20) ? "on " : "off", // Buzzer bit
+         (*regVal & 0x04) ? "on " : "off", // Middle Button bit
+         (*regVal & 0x01) ? "on " : "off", // Sensor bit
+         (*regVal & 0x02) ? "on " : "off", // Left Button bit
+         *currentTemp, unit);
+  return NULL;
+}
+
+void *T5(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {
+  int m_flag = (*regVal & 0x04);
+
+  int b_flag = (*regVal & 0x20);
+
+  if (m_flag) {
+    printf("Acknowledge: Middle Button On\n");
+
+    if (b_flag) {
+      printf("Acknowledge: Buzzer off\n");
+    } else {
+      printf("Acknowledge: Sensor on\n");
+    }
+  }
+}
+
+void *T6(double *currentTemp, int *exitLoop, char *tempVal, char *regVal) {
+  if (*regVal & 0x08) {
+    *exitLoop = 1;
+  }
+  return NULL;
+}
 
 int main() {
   // Shared data resources among all tasks and main
@@ -111,18 +172,22 @@ int main() {
   currentNode->nextNode = malloc(sizeof(fcnNode));
   currentNode = currentNode->nextNode;
   currentNode->fcnPtr = &T3;
+
   // Task 4
   currentNode->nextNode = malloc(sizeof(fcnNode));
   currentNode = currentNode->nextNode;
   currentNode->fcnPtr = &T4;
+
   // Task 5
   currentNode->nextNode = malloc(sizeof(fcnNode));
   currentNode = currentNode->nextNode;
   currentNode->fcnPtr = &T5;
+
   // Task 6
   currentNode->nextNode = malloc(sizeof(fcnNode));
   currentNode = currentNode->nextNode;
   currentNode->fcnPtr = &T6;
+
   // tail points to head and reset current node to head
   currentNode->nextNode = headerNode;
   currentNode = headerNode;
@@ -133,5 +198,11 @@ int main() {
     currentNode = currentNode->nextNode;
   }
   // add a goodbye message AND clean up you dynamically allocated memory!
+  for (int i = 0; i < 6; i++) {
+    fcnNode *temp = currentNode;
+    currentNode = currentNode->nextNode;
+    free(temp);
+  }
+  printf("Goodbye!\n");
   return 0;
 }
